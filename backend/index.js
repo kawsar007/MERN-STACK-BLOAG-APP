01269d4ed3903b4e4c80868751e419fcb1b4a816
unknown <kawsar007>
2021-09-06 11:19:16 +0600

    Create Models

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
 
-app.use('/lama', (req, res) => {
-    console.log('Hey this is lama url');
+// Connect Mongoose
+mongoose.connect(process.env.MONGODB_URL).then(console.log("Connect to MongoDB")).catch((err) => console.log(err));
+  
+// mongoose.connect(process.env.MONGODB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   //useCreateIndex: true,
+// })
+// .then(console.log("Connect to MongoDB"))
+// .catch((err) => console.log(err));
+
+
+app.use('/kawsar', (req, res) => {
+    console.log('Hey this is Kawsar url');
 })
 
-app.listen(5000, () => {
-  console.log(`Example app listening at http://localhost:5000`)
+app.listen("5000", () => {
+  console.log("Server is Running...")
 })
