08520849c203a96ec21b99f85b8bfe526005ce53
unknown <kawsar007>
2021-09-08 21:39:20 +0600

    create post, update post, delete post, get post

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,10 @@ app.use(express.json());
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 dotenv.config();
 
-
-// Connect Mongoose
-// mongoose.connect(process.env.MONGODB_URL).then(console.log("Connect to MongoDB")).catch((err) => console.log(err));
   
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
@@ -20,6 +19,8 @@ mongoose.connect(process.env.MONGODB_URL, {
 .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen("5000", () => {
   console.log("Server is Running...")
